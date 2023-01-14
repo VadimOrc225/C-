07e068881aac384b2149218aa6383c7498861010
Vadim <orc225@mail.ru>
2023-01-14 17:08:55 +0300

    initial commit

---
+++ less-c#/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите значение a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int y = b * b;
+if (a == y)   
+Console.Write($"да");
+else
+Console.Write($"нет");
