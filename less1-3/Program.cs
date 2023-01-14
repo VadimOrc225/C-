b1235238a5323c37d80b2d85ea1333e8b0d813cb
Vadim <orc225@mail.ru>
2023-01-14 16:36:26 +0300

    initial commit

---
+++ less1-3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите номер дня недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 1)   
+Console.Write($"понедельник");
+if (a == 2)
+Console.Write($"вторник");
+if (a == 3)
+Console.Write($"среда");
+if (a == 4)
+Console.Write($"четверг");
+if (a == 5)
+Console.Write($"пятница");
+if (a == 6)
+Console.Write($"суббота");
+if (a == 7)
+Console.Write($"воскресенье");
+if (a > 7)
+Console.Write($"ошибочный ввод");
+if (a < 1)
+Console.Write($"ошибочный ввод");
