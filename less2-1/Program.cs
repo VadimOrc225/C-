893f5b54b640a1a3bb008479f683fc39a19a07e6
Vadim <orc225@mail.ru>
2023-01-17 20:38:20 +0300

    first commit

---
+++ less2-1/Program.cs
@@ -0,0 +1,19 @@
+﻿int b = new Random().Next(10,100);
+int c = b % 10;
+float z = b / 10;
+
+if (z == c)
+{ 
+    Console.Write($"Cлучайное число {b} ");
+    Console.Write("Цифры равны ");
+}
+else if (z > c)
+{ 
+    Console.Write($"Cлучайное число {b} ");
+    Console.Write("Первая цифра больше ");
+}
+else
+{
+Console.Write($"Cлучайное число {b} ");
+Console.Write("Вторая цифра больше ");
+}
