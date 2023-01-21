c48ecb0e361a9e4743136fa2881dd25bebc1786b
Vadim <orc225@mail.ru>
2023-01-21 20:05:52 +0300

    После семенара 3

---
+++ zadacha4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел 
+// от 1 до N.
+Console.Write("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= N)
+{
+    int z = count*count;
+    Console.WriteLine(z);
+    count++;
+}
