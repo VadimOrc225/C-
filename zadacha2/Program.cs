acd50bdd7bb60a2ab5ff840daaf392ca78612a85
Vadim <orc225@mail.ru>
2023-01-21 20:05:12 +0300

    После семенара 3

---
+++ zadacha2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите номер четверти ");
+int z = Convert.ToInt32(Console.ReadLine());
+
+if (z == 3)
+{
+    Console.Write("Интервал координат: x<0 и y<0 ");
+}
+if (z == 4)
+{
+    Console.Write("Интервал координат: x<0 и y>0");
+}
+if (z == 1)
+{
+    Console.Write("Интервал координат: x>0 и y>0 ");
+}
+if (z == 2)
+{
+    Console.Write("Интервал координат: x>0 и y<0");
+}
